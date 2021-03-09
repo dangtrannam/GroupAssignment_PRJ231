b03e21bac734f2e4fd499ef50ed96a5b2e5d5440
OS <ngochc1@gmail.com>
2021-03-09 16:30:10 +0700

    updateDAo

---
+++ src/java/dtos/Quiz.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dtos;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author OS
+ */
+public class Quiz {
+    private ArrayList<Question> list;
+
+    public Quiz() {
+        list=new ArrayList();
+    }
+    public Quiz(ArrayList<Question> list) {
+        this.list = list;
+    }
+
+    public ArrayList<Question> getList() {
+        return list;
+    }
+
+    public void setList(ArrayList<Question> list) {
+        this.list = list;
+    }
+    
+}
